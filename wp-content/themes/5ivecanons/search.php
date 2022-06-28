83c18027d301b7cbde964ddb16907f633505fc51
mattloudenatrenet <matt@5ivecanons.com>
2022-06-28 09:53:22 -0400

    latest updates

--- wp-content/themes/5ivecanons/search.php
+++ wp-content/themes/5ivecanons/search.php
@@ -13,11 +13,153 @@ get_header();
 ?>
 <link rel="stylesheet" id="magnific-css"  href="<?php echo get_template_directory_uri().'/assets/css/magnific-popup.css'; ?>" type="text/css" media="all" />
 <link rel="stylesheet" id="sections-css"  href="<?php echo get_template_directory_uri().'/assets/css/sections.css'; ?>" type="text/css" media="all" />
+<style>
+	#layer-content .layer:before {
+		display: block;
+    position: absolute;
+    top: 25%;
+    left: -230px;
+    background-image: url(/wp-content/uploads/2022/06/search-left-blocks.svg);
+    content: '';
+    width: 382px;
+    height: 383px;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+    z-index: 1;
+	}
+	#layer-content .layer:after {
+		display: block;
+    position: absolute;
+    bottom: 10%;
+    right: -260px;
+    background-image: url(/wp-content/uploads/2022/06/search-bottom-blocks.svg);
+    content: '';
+    width: 435px;
+    height: 440px;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+    z-index: 1;
+	}
+	#layer-content .layer .container {
+		position: relative;
+		z-index: 2;
+	}
+	#layer-content .page-header {
+		padding: 40px 50px;
+		margin: 0 2% 40px;
+		width: 100%;
+		max-width: 100%;
+		border-bottom: 0;
+		display: flex;
+		flex-wrap: wrap;
+		align-items: center;
+		position: relative;
+	}
+	#layer-content .page-header > div {
+		position: relative;
+		z-index: 3;
+	}
+	#layer-content .page-header:before {
+		display: block;
+    position: absolute;
+    top: 50%;
+    right: 10%;
+    background-image: url(/wp-content/uploads/2022/06/searchbar-blocks.svg);
+    content: '';
+    width: 382px;
+    height: 383px;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+	transform: translatey(-50%);
+    z-index: 1;
+	}
+	#layer-content .page-header:after {
+		content: '';
+		backdrop-filter: blur(21.864px);
+	-webkit-backdrop-filter: blur(21.864px);
+    border-radius: 10px;
+    top: 0;
+    left: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+		background: rgb(24,24,24);
+background: linear-gradient(180deg, rgba(24,24,24,0.18) 0%, rgba(98,38,167,0.2) 100%);
+	}
+	.inner-header {
+		width: 40%;
+	}
+	.searchbar {
+		width: 50%;
+		margin-left: 10%;
+	}
+	#layer-content .page-header h1 {
+		color: #fff;
+		font-family: 'gotham';
+		font-weight: 500;
+		font-size: 30px;
+		text-transform: none;
+		margin-bottom: 0;
+	}
+	.search-term {
+		color: #78BE20;
+	}
+	.search-result-count {
+		color: #9D9D9D;
+		font-size: 20px;
+	}
+	.search-form {
+		flex-direction: row;
+		max-width: 100%;
+    width: 100%;
+	}
+	.search-form .search-field {
+		border: 1px solid #0C9DD9;
+    border-radius: 5px;
+    background: transparent;
+    color: #fff !important;
+    line-height: 1;
+    padding: 10px 20px;
+    width: calc(100% - 260px);
+    margin-bottom: 0px;
+		outline: none !important;
+	}
+	.attributes .date.page {
+		padding-left: 0;
+	}
+	.attributes .date.page:after {
+		display: none;
+	}
+	.news-wrapper.version2 {
+		justify-content: flex-start;
+	}
+	@media (max-width:992px){
+		.inner-header, .searchbar {
+			width: 100%;
+			margin-left: 0;
+		}
+		.search-result-count {
+			margin: 0;
+		}
+		.search-form {
+			flex-direction: column;
+			margin-top: 20px;
+			align-items: flex-start;
+		}
+		.search-form .search-field {
+			width: 100%;
+		}
+		.search-form .button {
+			margin-left: 0;
+		}
+	}
+</style>
 <div id="layer-content">
-<?php
-if ( have_posts() ) {
-	?>
-	<header class="page-header alignwide">
+<section class="layer">
+	<div class="container">
+		<header class="page-header alignwide">
+		<div class="inner-header">
 		<h1 class="page-title">
 			<?php
 			printf(
@@ -27,13 +169,10 @@ if ( have_posts() ) {
 			);
 			?>
 		</h1>
-	</header><!-- .page-header -->
-
-	<div class="search-result-count default-max-width">
+		<div class="search-result-count default-max-width">
 		<?php
 		printf(
 			esc_html(
-				/* translators: %d: The number of search results. */
 				_n(
 					'We found %d result for your search.',
 					'We found %d results for your search.',
@@ -44,9 +183,18 @@ if ( have_posts() ) {
 			(int) $wp_query->found_posts
 		);
 		?>
-	</div><!-- .search-result-count -->
+		</div><!-- .search-result-count -->
+		</div>
+		<div class="searchbar">
+			<?php get_search_form(); ?>
+		</div>
+	</header><!-- .page-header -->
+<?php
+if ( have_posts() ) {
+	?>
 	<?php
 	// Start the Loop.
+	$content .= '<div class="news-wrapper version2">';
 	while ( have_posts() ) {
 		the_post();
 
@@ -55,17 +203,43 @@ if ( have_posts() ) {
 		 * If you want to override this in a child theme, then include a file
 		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 		 */
-		get_template_part( 'template-parts/content/content-excerpt', get_post_format() );
+		//get_template_part( 'template-parts/content/content-excerpt', get_post_format() );
+		$content .= '<article class="news-tile">';
+			if(get_the_post_thumbnail()){
+				$content .= '<div class="news-tile-image"><img src="'.get_the_post_thumbnail_url().'" alt="'.get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true ).'"></div>';
+			}else{
+				$content .= '<div class="news-tile-image"><img src="/wp-content/uploads/2022/05/Xperi-Featured-Image.jpg" alt="Page Image"></div>';
+			}
+			$category = get_the_category(); 
+			$catname = $category[0]->cat_name;
+		$content .= '<div class="news-tile-content">';
+		if(get_post_type() == 'page'){
+			$content .= '<div class="attributes">
+<span class="date page">Page</span>
+</div>';
+		}else{
+			$content .= '<div class="attributes">
+<span class="date">'.get_the_date('F j, Y').'</span>
+<span class="category">'.$catname.'</span>
+</div>';
+		}
+			$content .= '<h4 class="news-tile-title">'.wp_trim_words(get_the_title(), 7, '...').'</h4>';
+			$content .= '<div class="news-tile-excerpt">'.wp_trim_words(get_the_content(), 25, '...').'</div>';
+			$content .= '<div class="news-tile-cta"><a href="'.get_permalink().'" class="btn button">Read More</a></div></div>';
+			$content .= '</article>';
 	} // End the loop.
-
+	$content .= '</div>';
+	echo $content;
 	// Previous/next page navigation.
 	twenty_twenty_one_the_posts_navigation();
 
 	// If no content, include the "No posts found" template.
 } else {
-	get_template_part( 'template-parts/content/content-none' );
+	//get_template_part( 'template-parts/content/content-none' );
 }
 ?>
+</div>
+</section>
 </div>
 <?php
 get_footer();
--- wp-content/themes/5ivecanons/single.php
+++ wp-content/themes/5ivecanons/single.php
@@ -10,30 +10,33 @@
  */
 
 get_header();
-
+?>
+<link rel="stylesheet" id="magnific-css"  href="<?php echo get_template_directory_uri().'/assets/css/magnific-popup.css'; ?>" type="text/css" media="all" />
+<link rel="stylesheet" id="sections-css"  href="<?php echo get_template_directory_uri().'/assets/css/sections.css'; ?>" type="text/css" media="all" />
+<?php
 /* Start the Loop */
 while ( have_posts() ) :
 	the_post();
 
 	get_template_part( 'template-parts/content/content-single' );
 
-	if ( is_attachment() ) {
+	/* if ( is_attachment() ) {
 		// Parent post navigation.
 		the_post_navigation(
 			array(
-				/* translators: %s: Parent post link. */
+				/* translators: %s: Parent post link. 
 				'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'twentytwentyone' ), '%title' ),
 			)
 		);
-	}
+	}*/
 
 	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
+	/*if ( comments_open() || get_comments_number() ) {
 		comments_template();
-	}
+	}*/
 
 	// Previous/next post navigation.
-	$twentytwentyone_next = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' );
+	/*$twentytwentyone_next = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' );
 	$twentytwentyone_prev = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' );
 
 	$twentytwentyone_next_label     = esc_html__( 'Next post', 'twentytwentyone' );
@@ -44,7 +47,7 @@ while ( have_posts() ) :
 			'next_text' => '<p class="meta-nav">' . $twentytwentyone_next_label . $twentytwentyone_next . '</p><p class="post-title">%title</p>',
 			'prev_text' => '<p class="meta-nav">' . $twentytwentyone_prev . $twentytwentyone_previous_label . '</p><p class="post-title">%title</p>',
 		)
-	);
+	);*/
 endwhile; // End of the loop.
 
 get_footer();
